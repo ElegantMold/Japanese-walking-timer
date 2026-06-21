4ce730beaa9a4dfb23c38c62cd72cfa338bbae7e
ElegantMold <nealallen50@gmail.com>
2026-06-21 10:53:57 -0700

    Refresh app cache for audio cues

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,35 @@
+const CACHE_NAME = "japanese-walking-timer-v5";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./service-worker.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) =>
+      cachedResponse || fetch(event.request)
+    )
+  );
+});
 const CACHE_NAME = "japanese-walking-timer-v3";
 const APP_FILES = [
   "./",
