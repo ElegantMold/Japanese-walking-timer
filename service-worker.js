103024e7e0233eb926098f605c775e3a008b417a
ElegantMold <nealallen50@gmail.com>
2026-06-21 10:55:20 -0700

    Fix service worker cache

--- service-worker.js
+++ service-worker.js
@@ -30,35 +30,3 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
-const CACHE_NAME = "japanese-walking-timer-v3";
-const APP_FILES = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icon.svg",
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)))
-    )
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) =>
-      cachedResponse || fetch(event.request)
-    )
-  );
-});
-
